89e96c10367e0f896cc04642e411fe9aa37a29d6
bestarti99 <praca.uek@outlook.com>
2017-06-25 11:07:02 +0200

    koniec

--- js/script.js
+++ js/script.js
@@ -1,23 +1,82 @@
 'use strict'
 
-var slider = $('#slider');
-var slideShow = $('.slide-show');
-var slideCount = $('.slide-show').length;
-console.log($('.slide-show').length);
 
-var slideWidth = slideCount/100;
-var slideIndex = 0;
+$(document).ready(function () {
+    // Zmienne
+    var slider = $('#slider');
+    var slideShow = $('#slider .slide-show');
 
-$('.slide-show').css('slideCount', '400%');
+    // liczenie sldów j
+    var slideCount = slideShow.children().length;
+    //console.log(slideCount);
 
-$('.slideShow').find('.single-slide');
-$('.single-slide').each(function(index){
-    
+    /*
+    //wersja druga 
+    var slideCount = $('.single-slide').length;
+    console.log(slideCount);
+    */
+
+    var slideWidth = 100 / slideCount;
+    //console.log(slideWidth);
+
+    var slideIndex = 0;
+
+    // ustawienie szerokości kontenera
+    slideShow.css('width', slideCount * 100 + '%');
+
+    // Ustawienie marginesów i szerokości dla pojedynczychslajdów 
+    slideShow.find('.single-slide').each(function (index) {
+        var leftPercent = (slideWidth * index) + '%';
+        $(this).css('margin-left', leftPercent);
+        $(this).css('width', slideWidth + '%');
+    })
+
+    // Wywołanie funkcji slide na click
+    $('.prev-slide').click(function(event) {
+        event.preventDefault();
+        slide(slideIndex - 1);
+    })
+
+    $('.next-slide').click(function(event) {
+        event.preventDefault();
+        slide(slideIndex + 1);
+    })
+
+    // Definicja funkcji slide
+    function slide(newSlideIndex) {
+        // Sprawdzamy czy pierwszy, czy ostatni slajd i wtedy nic nie robimy
+        if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
+            return;
+        }
+
+        // elemnty slideCaption przypisanie do funkcji
+        var slideCaption = slider.find('.slider-caption').eq(newSlideIndex);
+
+        // Ukrywanie napisu
+        slideCaption.hide();
+        
+        // Zmienna trzymająca lewy margines do przesówania kontenera
+        var marginLeft = (newSlideIndex * (-100)) + '%';
+        
+        // Animacja na slideShow
+        slideShow.animate({'margin-left': marginLeft}, 800, function() {
+            // prxypisanie do slideIndex nowego indeksu slajdu
+            slideIndex = newSlideIndex;
+            
+            // pokazanie napisu przez fadeIn()
+            slideCaption.fadeIn('slow');
+            
+            
+            
+        })
+
+
+
+
+    }
+
+
+
+
+
 })
-
-
-
-
-    
-
-/*4.	W obiekcie slideShow za pomocą metody find() slide(.single-slide) i dla każdego (metoda each() - https://api.jquery.com/each/) wykonaj funkcję, która przypisze elementowi .single-slide lewy margines w procentach (szerokość slidu razy indeks) oraz szerokość w procentach (dla 5 slidów to 100%/5 czyli 20%)*/
